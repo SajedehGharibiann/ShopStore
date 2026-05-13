8653c34d5e635cfa07c5b4c522b8561a65ef89ff
sajedegharibian <sajedegharibian@gmail.com>
2026-05-13 23:04:10 +0330

    Update index.jsx

--- Front-End/src/Pages/index.jsx
+++ Front-End/src/Pages/index.jsx
@@ -3,11 +3,10 @@ import Cart from "./Cart";
 import Profile from "./Profile";
 import Auth from "./Auth";
 import Product from "./Products";
-import ProductsDetails from "./ProductsDetails";
 import About from "./About";
 import Contact from "./Contact";
 import NotFound from "./NotFound";
 
 
 
-export {Home,Cart,Profile,Auth,Product,ProductsDetails,About,Contact,NotFound}
+export {Home,Cart,Profile,Auth,Product,About,Contact,NotFound}
